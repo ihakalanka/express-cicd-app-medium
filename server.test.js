6ec116496c2a73ad683d7791f207b918f0caaaea
ihakalanka <akalankaih19@gmail.com>
2024-05-26 02:40:29 +0530

    fixed

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const app = require('./index');
+const app = require('./server');
 
 describe('GET /', () => {
     it('should return Hello World', async () => {
